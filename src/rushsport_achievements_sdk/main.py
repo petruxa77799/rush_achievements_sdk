252d4630f2de9ef9d4eba0a451fe6cb195f726e6
petruxa <petrscherbakov93@gmail.com>
2022-08-16 22:41:19 +0700

    Upd

--- src/rushsport_achievements_sdk/main.py
+++ src/rushsport_achievements_sdk/main.py
@@ -19,27 +19,11 @@ class AchievementsSDK:
         while True:
             data: dict = await self.queues.send_achievements.get()
             try:
-                async with self.db_pool.acquire() as con:
-                    event_achievement = await con.fetchval(
-                        """
-                        SELECT ea.value
-                        FROM eventachievement as ea
-                        JOIN achievement ach on ach.id = ea.achievement_id
-                        WHERE ea.event_id=$1 AND ach.team_id=$2 AND ea.trigger_type=$3
-                        """, data['event_id'], data['team_id'], data['trigger_type'])
-                if event_achievement:
-                    data['value'] = event_achievement['value']
-                    await self.__send_trigger(achievements_data=data)
+                await self.aio_session.post(f'http://{self.host}/api/achievements/',
+                                            json=data, ssl=False)
             except Exception as e:
                 self.logger.exception(f'Problem send_achievements_data. Exception: {e}')
                 continue
-
-    async def __send_trigger(self, achievements_data):
-        try:
-            await self.aio_session.post(f'http://{self.host}/api/achievements/',
-                                        json=achievements_data, ssl=False)
-        except Exception as e:
-            self.logger.exception(f'Problem with send trigger to achievements service. Exception: {e}')
 
     async def send_achievement(self, achievements_data):
         await self.queues.send_achievements.put(achievements_data)
