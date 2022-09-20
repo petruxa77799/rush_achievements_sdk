bcd754f7f5c85a1dfb4f4165ad59dd65c6c57a0f
petruxa <petrscherbakov93@gmail.com>
2022-09-20 15:11:14 +0700

    Aiohttp session context manager

--- src/rushsport_achievements_sdk/main.py
+++ src/rushsport_achievements_sdk/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+from typing import List
 
 from aiohttp import ClientSession
 
@@ -8,22 +9,28 @@ from .choices import TriggerTypes
 
 
 class AchievementsSDK:
-    def __init__(self, achievements_host: str, aio_session: ClientSession, workers: int):
+    def __init__(self, achievements_host: str, workers: int):
         self.__logger = logging.getLogger(__name__)
         self.__host = achievements_host
-        self.__aio_session = aio_session
         self.__queues = Queues(send_achievements=asyncio.Queue())
-        for _ in range(workers):
-            asyncio.create_task(self.__send_achievements_data())
+        self.__tasks = []
+        self.__workers = workers
+        for i in range(workers):
+            task = asyncio.create_task(self.__send_achievements_data(i))
+            self.__tasks.append(task)
 
-    async def __send_achievements_data(self):
+    async def __send_achievements_data(self, number):
         while True:
-            data: dict = await self.__queues.send_achievements.get()
-            try:
-                await self.__aio_session.post(f'http://{self.__host}/api/achievements/',
-                                              json=data, ssl=False)
-            except Exception as e:
-                self.__logger.exception(f'Problem send_achievements_data. Exception: {e}')
+            async with ClientSession() as session:
+                data: dict = await self.__queues.send_achievements.get()
+                if not data:
+                    self.__logger.info(f"Task {number} was done!")
+                    return
+                try:
+                    await session.post(f'http://{self.__host}/api/achievements/',
+                                       json=data, ssl=False)
+                except Exception as e:
+                    self.__logger.exception(f'Problem send_achievements_data. Exception: {e}')
 
     async def send_user_plays_market_achievement(self, user_id: int, team_id: int, event_id: int, value: int = None):
         await self.__queues.send_achievements.put({
@@ -47,3 +54,16 @@ class AchievementsSDK:
             'clock': clock,
             'trigger_type': TriggerTypes.QUARTER_CLOCK
         })
+
+    async def close(self):
+        async def check_shutdown_tasks(tasks: List[asyncio.Task]):
+            for task in tasks:
+                if task.done():
+                    self.__tasks.remove(task)
+                    return
+            await asyncio.sleep(1)
+            await check_shutdown_tasks(tasks=tasks)
+
+        for i in range(self.__workers):
+            await self.__queues.send_achievements.put(None)
+            await check_shutdown_tasks(tasks=self.__tasks)
